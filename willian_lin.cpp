426c0a69cbd5151ee6a39e306e34ea942f769ed7
fuji <fuji@skymizer.com>
2021-12-06 17:02:19 +0800

    Add read file sample

--- willian_lin.cpp
+++ willian_lin.cpp
@@ -1,12 +1,18 @@
 #include "common/utils.hpp"
 void solve()
 {
+    ll n, c, d, f;
+    read(n, c, d, f);
+    string s;
+    read(s);
+    print(n, c, d, f);
 }
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
     int t = 1;
     read(t);
     FOR(t)
